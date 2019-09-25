9415b5bb6bf76c7c74ce6c659236ee5618894b62
Joran Quinten <joran.quinten@jumbo.com>
2019-09-25 13:05:58 +0200

    feat(): adds author

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div id="app">
     <DrumMachine />
+    <footer>
+      Built with ❤️ by
+      <a
+        href="https://joranquinten.nl"
+        title="Joran Quinten, freelance webdeveloper"
+        >Joran Quinten</a
+      >
+    </footer>
   </div>
 </template>
 
@@ -15,27 +23,30 @@ export default {
 };
 </script>
 
-<style>
-html {
-  background: linear-gradient(
-      to bottom,
-      rgba(100, 100, 100, 0.15) 0%,
-      rgba(0, 0, 0, 0.15) 100%
-    ),
-    radial-gradient(
-        at top center,
-        rgba(50, 50, 50, 0.4) 0%,
-        rgba(0, 0, 0, 0.4) 120%
-      )
-      #333;
-  background-blend-mode: multiply, multiply;
+<style lang="scss">
+html,
+html body {
+  background: #111;
 }
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+
+  footer {
+    position: absolute;
+    width: 100%;
+    bottom: 0;
+    text-align: center;
+    color: #cfcfcf;
+    padding: 0.4em 0;
+
+    a {
+      color: #cfcfcf;
+    }
+  }
 }
 </style>
